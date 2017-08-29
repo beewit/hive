b07c30edd57b24148e639c18adf97ca4bece4e27
zxbit2011 <zxbit2011@163.com>
2017-08-29 15:27:39 +0800

    --

--- router/router.go
+++ router/router.go
@@ -16,7 +16,7 @@ func Start() {
 
 	e := echo.New()
 	e.Use(middleware.Gzip())
-	e.Use(middleware.Logger())
+	//e.Use(middleware.Logger())
 	e.Use(middleware.CSRF())
 	e.Use(middleware.CORS())
 	e.Use(middleware.Recover())
@@ -29,7 +29,22 @@ func Start() {
 	e.GET("/api/template", handler.GetTemplateByList)
 	e.GET("/api/template/id", handler.GetTemplateById)
 
+	e.GET("test", jsonTest)
+
 	utils.Open(global.Host)
 
 	e.Logger.Fatal(e.Start(":8090"))
+}
+
+func jsonTest(c echo.Context) error {
+
+	maps2 := make(map[string]interface{})
+	maps2["id"] = 1
+	maps2["name"] = "张三"
+	maps := make(map[string]interface{})
+	maps["id"] = 1
+	maps["name"] = "张三"
+
+	maps2["list"]=maps
+	return  utils.Success(c, "", maps2)
 }
